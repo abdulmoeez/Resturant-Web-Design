f376cefcf77c877ccf195fd8edcb18348b1a51b4
Abdul Moeez <flakerocks71@gmail.com>
2019-10-27 00:32:25 +0500

    media queries updated for responsove design

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // SELECT ELEMENTS
 var pizzaMenu = document.querySelector(".menuPizzas");
 var BurgerMenu = document.querySelector(".menuBurgers");
@@ -7,6 +8,8 @@ var burgerBtn = document.querySelector(".menuBurgerBtn");
 var pizzaBtn = document.querySelector(".menuPizzaBtn");
 var drinkBtn = document.querySelector(".menuDrinkBtn");
 var saladBtn = document.querySelector(".menuSaladBtn");
+
+
 
 function hideMenu(menuHide1,menuHide2,menuHide3, menuShow,btn1,btn2,btn3,btnShow){
     menuHide1.style.display = 'none';
@@ -34,13 +37,25 @@ function burger(){
 
 function drink(){
     hideMenu(saladMenu,pizzaMenu,BurgerMenu,drinksMenu,burgerBtn,saladBtn,pizzaBtn,drinkBtn);
-    
-
 }
 
 function salad(){
     hideMenu(drinksMenu,pizzaMenu,BurgerMenu,saladMenu,burgerBtn,drinkBtn,pizzaBtn,saladBtn);
-    
-
 }
 
+
+function navbarActive(){
+    document.querySelector("nav").style.display = 'block';
+    document.querySelector(".hamburgerIcon").style.display = 'none';
+}
+
+
+function navbarHide(){
+    document.querySelector("nav").style.display = 'none';
+    document.querySelector(".hamburgerIcon").style.display = 'block';
+}
+
+
+    document.querySelector(".navbarHide").addEventListener("click", navbarHide);
+
+    
